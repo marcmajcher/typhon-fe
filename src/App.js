f310d887998a1551fdd3e64df306874f83e6169a
Marc Majcher <majcher@gmail.com>
2020-09-14 13:00:43 -0500

    testing endpoint

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { checkToken } from './actions';
-import TopNav from './components/TopNav'
+import TopNav from './components/TopNav';
 import TestConsole from './components/TestConsole';
 
 export default function App() {
--- src/components/TestConsole.js
+++ src/components/TestConsole.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 export default function TestConsole() {
   // const token = useSelector(s => s.token);
   const userInfo = useSelector(s => s.userInfo);
+  const endpoint = useSelector(s => s.endpoint);
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    axios(`${endpoint}/users`).then(res => {
+      console.log(res.data);
+      setUserList(res.data);
+    });
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return <div className="console">
     <div><b>Test Console</b></div>
     <ul>
       {/* < li>Token: {token}</li> */}
       <li>UserInfo: {JSON.stringify(userInfo)}</li>
+      <li>Users: {JSON.stringify(userList)}</li>
     </ul>
   </div>;
 }

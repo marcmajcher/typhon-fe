b50cf1c8a94ab2d4fa8666893d32580a7e9dcd57
Marc Majcher <majcher@gmail.com>
2020-09-15 08:24:38 -0500

    user image

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import LoginButton from './LoginButton';
 
 export default function TopNav() {
+  const userInfo = useSelector(s => s.userInfo);
 
   return <div className="top-nav">
     <h1>Typhon Station</h1>
     <LoginButton />
+    {userInfo ?
+      <p>
+        <img className="user-image" src={userInfo.imageUrl} alt="user" />
+        Wassup, {userInfo.firstName}?
+      </p>
+      : <p> Please log in.</p>}
   </div>;
 }

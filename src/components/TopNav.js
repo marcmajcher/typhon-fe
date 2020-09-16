da57f19972c93b13f8fa962576bb7cb1b9b7daff
Marc Majcher <majcher@gmail.com>
2020-09-16 17:13:07 -0500

    nav font

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -7,8 +7,8 @@ export default function TopNav() {
   const userInfo = useSelector(s => s.userInfo);
 
   return <nav className="top-nav">
-    <img class="logo-img" src="defense-satellite-green.png" alt="Typhon Station" />
-    <h1>Typhon Station</h1>
+    {/* <img class="logo-img" src="defense-satellite-green.png" alt="Typhon Station" /> */}
+    <h1 class="title">TYPHON STATION</h1>
     {userInfo ?
         <img className="user-image" src={userInfo.imageUrl} alt="user" />
       : ''}

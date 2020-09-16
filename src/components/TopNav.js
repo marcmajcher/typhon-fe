a4e07a82ea38e461a9aabcbbfbf78b599ed45997
Marc Majcher <majcher@gmail.com>
2020-09-16 17:00:19 -0500

    logo

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -7,6 +7,7 @@ export default function TopNav() {
   const userInfo = useSelector(s => s.userInfo);
 
   return <nav className="top-nav">
+    <img class="logo-img" src="defense-satellite-green.png" alt="Typhon Station" />
     <h1>Typhon Station</h1>
     {userInfo ?
         <img className="user-image" src={userInfo.imageUrl} alt="user" />
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {Provider} from 'react-redux';
-import {store} from './redux-store';
+import { Provider } from 'react-redux';
+import { store } from './redux-store';
 import 'react-skeleton-css/styles/skeleton.2.0.4.css';
 import './index.css';
 

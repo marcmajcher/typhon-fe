a91290520c35d8be69d05b85db78da7fb36b76e8
Marc Majcher <majcher@gmail.com>
2020-09-16 23:50:25 -0500

    getting flow down

--- src/data/pilotData.js
+++ src/data/pilotData.js
@@ -1,30 +1,90 @@
 export default {
-  species: {
-    1: { id: 1, name: 'Nahmra', occupations: [1, 2, 3, 4] },
-    2: { id: 2, name: 'Korazz', occupations: [5, 6, 7, 8] },
-    3: { id: 3, name: 'Runian', occupations: [9, 10, 11, 12] },
-    4: { id: 4, name: 'Wyl', occupations: [13, 14, 15, 16] }
-  },
-  occupations: {
-    1: { id: 1, name: 'VR Promoter', attitude: 'Volatile', tactics: 'Efficient', interests: 'Social', structure: 'Stratified' },
-    2: { id: 2, name: 'Stim Dealer', attitude: 'Disciplined', tactics: 'Detached', interests: 'Hedonic', structure: 'Autarchic' },
-    3: { id: 3, name: 'Political Agitator', attitude: 'Adaptable', tactics: 'Intimate', interests: 'Economic', structure: 'Communal' },
-    4: { id: 4, name: 'Bounty Hunter', attitude: 'Confident', tactics: 'Casual', interests: 'Martial', structure: 'Solitary' },
-    5: { id: 5, name: 'Troubleshooter', attitude: 'Relentless', tactics: 'Focused', interests: 'Technical', structure: 'Bonded' },
-    6: { id: 6, name: 'Bookie', attitude: 'Volatile', tactics: 'Detached', interests: 'Economic', structure: 'Solitary' },
-    7: { id: 7, name: 'Mechanic', attitude: 'Disciplined', tactics: 'Casual', interests: 'Technical', structure: 'Communal' },
-    8: { id: 8, name: 'Assassin', attitude: 'Adaptable', tactics: 'Efficient', interests: 'Martial', structure: 'Autarchic' },
-    9: { id: 9, name: 'Socialite', attitude: 'Relentless', tactics: 'Focused', interests: 'Hedonic', structure: 'Stratified' },
-    10: { id: 10, name: 'Personal Assistant', attitude: 'Confident', tactics: 'Intimate', interests: 'Social', structure: 'Bonded' },
-    11: { id: 11, name: 'Implant Technician', attitude: 'Relentless', tactics: 'Efficient', interests: 'Hedonic', structure: 'Autarchic' },
-    12: { id: 12, name: 'Gladiator', attitude: 'Volatile', tactics: 'Intimate', interests: 'Martial', structure: 'Stratified' },
-    13: { id: 13, name: 'Cybersecurity', attitude: 'Adaptable', tactics: 'Focused', interests: 'Technical', structure: 'Solitary' },
-    14: { id: 14, name: 'Philanthropist', attitude: 'Disciplined', tactics: 'Detached', interests: 'Economic', structure: 'Bonded' },
-    15: { id: 15, name: 'Cult Leader', attitude: 'Confident', tactics: 'Casual', interests: 'Social', structure: 'Communal' },
-    16: { id: 16, name: 'Bodyguard', attitude: 'Disciplined', tactics: 'Intimate', interests: 'Martial', structure: 'Autarchic' },
-    17: { id: 17, name: 'Life Coach', attitude: 'Confident', tactics: 'Focused', interests: 'Social', structure: 'Communal' },
-    18: { id: 18, name: 'Circus Performer', attitude: 'Volatile', tactics: 'Efficient', interests: 'Economic', structure: 'Bonded' },
-    19: { id: 19, name: 'Military Programmer', attitude: 'Relentless', tactics: 'Detached', interests: 'Technical', structure: 'Stratified' },
-    20: { id: 20, name: 'Confidence Artist', attitude: 'Adaptable', tactics: 'Casual', interests: 'Hedonic', structure: 'Solitary' },
+  species: [
+    {
+      id: 1, name: 'Nahmra',
+      description: 'monkeys',
+      occupation: [1, 2, 3, 4, 5],
+      lineage: [1, 2, 3],
+      gender: [1, 2, 4],
+    },
+    {
+      id: 2, name: 'Korazz',
+      description: 'bugs',
+      occupation: [6, 7, 8, 9, 10],
+      lineage: [4, 5, 6],
+      gender: [5, 6, 7, 8]
+    },
+    {
+      id: 3, name: 'Runian',
+      description: 'rocks',
+      occupation: [11, 12, 13, 14, 15],
+      lineage: [7, 8, 9],
+      gender: [9]
+    },
+    {
+      id: 4, name: 'Wyl',
+      description: 'gas',
+      occupation: [16, 17, 18, 19, 20],
+      lineage: [10, 11, 12],
+      gender: [10, 11, 12, 13, 14, 15]
+    }
+  ],
+  occupation: [
+    { id: 1, name: 'VR Promoter', attitude: 'Volatile', tactics: 'Efficient', interests: 'Social', structure: 'Stratified' },
+    { id: 2, name: 'Stim Dealer', attitude: 'Disciplined', tactics: 'Detached', interests: 'Hedonic', structure: 'Autarchic' },
+    { id: 3, name: 'Political Agitator', attitude: 'Adaptable', tactics: 'Intimate', interests: 'Economic', structure: 'Communal' },
+    { id: 4, name: 'Bounty Hunter', attitude: 'Confident', tactics: 'Casual', interests: 'Martial', structure: 'Solitary' },
+    { id: 5, name: 'Troubleshooter', attitude: 'Relentless', tactics: 'Focused', interests: 'Technical', structure: 'Bonded' },
+    { id: 6, name: 'Bookie', attitude: 'Volatile', tactics: 'Detached', interests: 'Economic', structure: 'Solitary' },
+    { id: 7, name: 'Mechanic', attitude: 'Disciplined', tactics: 'Casual', interests: 'Technical', structure: 'Communal' },
+    { id: 8, name: 'Assassin', attitude: 'Adaptable', tactics: 'Efficient', interests: 'Martial', structure: 'Autarchic' },
+    { id: 9, name: 'Socialite', attitude: 'Relentless', tactics: 'Focused', interests: 'Hedonic', structure: 'Stratified' },
+    { id: 10, name: 'Personal Assistant', attitude: 'Confident', tactics: 'Intimate', interests: 'Social', structure: 'Bonded' },
+    { id: 11, name: 'Implant Technician', attitude: 'Relentless', tactics: 'Efficient', interests: 'Hedonic', structure: 'Autarchic' },
+    { id: 12, name: 'Gladiator', attitude: 'Volatile', tactics: 'Intimate', interests: 'Martial', structure: 'Stratified' },
+    { id: 13, name: 'Cybersecurity', attitude: 'Adaptable', tactics: 'Focused', interests: 'Technical', structure: 'Solitary' },
+    { id: 14, name: 'Philanthropist', attitude: 'Disciplined', tactics: 'Detached', interests: 'Economic', structure: 'Bonded' },
+    { id: 15, name: 'Cult Leader', attitude: 'Confident', tactics: 'Casual', interests: 'Social', structure: 'Communal' },
+    { id: 16, name: 'Bodyguard', attitude: 'Disciplined', tactics: 'Intimate', interests: 'Martial', structure: 'Autarchic' },
+    { id: 17, name: 'Life Coach', attitude: 'Confident', tactics: 'Focused', interests: 'Social', structure: 'Communal' },
+    { id: 18, name: 'Circus Performer', attitude: 'Volatile', tactics: 'Efficient', interests: 'Economic', structure: 'Bonded' },
+    { id: 19, name: 'Military Programmer', attitude: 'Relentless', tactics: 'Detached', interests: 'Technical', structure: 'Stratified' },
+    { id: 20, name: 'Confidence Artist', attitude: 'Adaptable', tactics: 'Casual', interests: 'Hedonic', structure: 'Solitary' },
+  ],
+  lineage: [
+    { id: 1, name: 'Troop1' },
+    { id: 2, name: 'Troop2' },
+    { id: 3, name: 'Troop3' },
+    { id: 4, name: 'Hive1' },
+    { id: 5, name: 'HIve2' },
+    { id: 6, name: 'Hive3' },
+    { id: 7, name: 'Quarry1' },
+    { id: 8, name: 'Quarry2' },
+    { id: 9, name: 'Quarry3' },
+    { id: 10, name: 'Cloud1' },
+    { id: 11, name: 'Cloud2' },
+    { id: 12, name: 'Cloud3' },
+  ],
+  gender: [
+    { id: 1, name: 'male' },
+    { id: 2, name: 'female' },
+    { id: 4, name: 'agender' },
+    { id: 5, name: 'worker' },
+    { id: 6, name: 'soldier' },
+    { id: 7, name: 'major' },
+    { id: 8, name: 'minor' },
+    { id: 9, name: 'crystal' },
+    { id: 10, name: 'vermillion' },
+    { id: 11, name: 'amber' },
+    { id: 12, name: 'chartreuse' },
+    { id: 13, name: 'teal' },
+    { id: 14, name: 'violet' },
+    { id: 15, name: 'indigo' },
+  ],
+
+  getById: function (key, id) { return this[key].find(e => e.id === parseInt(id)); },
+  bySpecies: function (key, sid) {
+    return sid && this[key].filter(e => this.getById('species', sid)[key].includes(e.id));
   }
 };
+
--- src/flows/NewPilotFlow.js
+++ src/flows/NewPilotFlow.js
@@ -2,44 +2,40 @@ import React, { useState } from 'react';
 import NewPilotStep1 from './NewPilotStep1';
 import NewPilotStep2 from './NewPilotStep2';
 import './NewPilotFlow.css';
-import pilotData from '../data/pilotData';
+import data from '../data/pilotData';
 
 export default function NewPilotFlow() {
   const [pilotInfo, setPilotInfo] = useState({});
   const [step, setStep] = useState(0);
-  const [trail, setTrail] = useState([]);
 
+  const fields = ['species', 'occupation', 'gender', 'lineage', 'appearance', 'keepsake'];
+  const trail = fields.filter(e => pilotInfo.hasOwnProperty(e)).map(e => data.getById(e, pilotInfo[e]).name).join(' > ');
   const steps = [
-    <NewPilotStep1 species={Object.values(pilotData.species)}
-      setSpecies={setSpecies} nextStep={nextStep} />,
-    <NewPilotStep2 occupations={pilotData.species[pilotInfo.species].occupations.map(o => pilotData.occupations[o])}
-      setOccupation={setOccupation} nextStep={nextStep} previousStep={previousStep} />,
+    <NewPilotStep1 species={data.species} setSpecies={species => setField('species', species)} nextStep={nextStep} />,
+    <NewPilotStep2 occupations={data.bySpecies('occupation',pilotInfo.species)}
+      setOccupation={occ => setField('occupation', occ)} nextStep={nextStep} previousStep={previousStep} />,
   ];
 
-  function setSpecies(speciesId) {
-    setPilotInfo({ ...pilotInfo, speciesId });
-    setTrail([pilotData.species[speciesId].name]);
-  }
-
-  function setOccupation(occupationId) {
-    setPilotInfo({ ...pilotInfo, occupationId });
-    setTrail([
-      pilotData.species[pilotInfo.species].name,
-      pilotData.occupation[pilotInfo.occupation].name,
-    ]);
+  function setField(field, value) {
+    setPilotInfo({ ...pilotInfo, [field]: value });
   }
 
   function nextStep() {
     setStep(Math.min(step + 1, steps.length - 1));
   }
+
   function previousStep() {
+    const newPilotInfo = { ...pilotInfo };
+    delete newPilotInfo[fields[step]];
+    setPilotInfo(newPilotInfo);
     setStep(Math.max(step - 1, 0));
   }
+
 
   return <div>
     <h2>Looks like you're new here. Let's get you a new pilot.</h2>
 
-    <div><b>:: {trail.join(' > ')}</b></div>
+    <div><b>:: {trail}</b></div>
 
     {steps[step]}
   </div >;

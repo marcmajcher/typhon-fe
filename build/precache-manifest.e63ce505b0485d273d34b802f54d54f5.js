bd19f8182af901f736e4b1d589ef3c7593d14af3
Marc Majcher <majcher@gmail.com>
2021-05-27 23:27:01 -0500

    build

---
+++ build/precache-manifest.e63ce505b0485d273d34b802f54d54f5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea80536fb05a40742c15ee69ba962a85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5dbfe56813bcf7e37e0b",
+    "url": "/static/css/main.6a308b61.chunk.css"
+  },
+  {
+    "revision": "ec75f06577b64a45ec2b",
+    "url": "/static/js/2.2b1488fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2b1488fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dbfe56813bcf7e37e0b",
+    "url": "/static/js/main.c2e3febc.chunk.js"
+  },
+  {
+    "revision": "cf2ae43b284528932e3c",
+    "url": "/static/js/runtime-main.033a87f6.js"
+  },
+  {
+    "revision": "efa63de0d44af1e7de9e01a4467dd423",
+    "url": "/static/media/SourceCodePro-Black.efa63de0.ttf"
+  },
+  {
+    "revision": "f621c504d70317ff13774e27d643ba21",
+    "url": "/static/media/SourceCodePro-Medium.f621c504.ttf"
+  }
+]);
